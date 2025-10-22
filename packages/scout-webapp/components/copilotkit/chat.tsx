8634210ac84a2ece063f48a49114d3e1230a5a03
Dao Hoang Son <daohoangson@gmail.com>
2025-10-22 14:35:29 +0700

    feat: hide suggestions after the first message

--- packages/scout-webapp/components/copilotkit/chat.tsx
+++ packages/scout-webapp/components/copilotkit/chat.tsx
@@ -2,44 +2,61 @@
 import "@copilotkit/react-ui/styles.css";
 
 import { CopilotChat } from "@copilotkit/react-ui";
-import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotKit, useCopilotChat } from "@copilotkit/react-core";
 import { AGENT_ID_TEST_LEAD_AGENT } from "@scoutqa-dot-ai/scout-agent/src/config/constants";
+import { useMemo } from "react";
 import { RenderMessage } from "./render-message";
 
-export function Chat() {
+const ChatInner = () => {
+  const { visibleMessages } = useCopilotChat();
+  const visibleMessagesLength = visibleMessages.length;
+  const suggestions = useMemo(() => {
+    if (visibleMessagesLength === 0) {
+      return [
+        {
+          title: "AirBnb",
+          message: "Verify booking flow on airbnb.com",
+        },
+        {
+          title: "Google",
+          message: "Make sure user can see latest weather on google.com",
+        },
+        {
+          title: "Medium",
+          message:
+            "Make sure user can follow links on https://medium.com/blog/32-of-our-favorite-medium-stories-of-2023-1fb10ca34cd8",
+        },
+        {
+          title: "Temu",
+          message: "Testing buying a hoodie on temu.com",
+        },
+      ];
+    }
+
+    return [];
+  }, [visibleMessagesLength]);
+
+  return (
+    <CopilotChat
+      className="h-full"
+      RenderMessage={RenderMessage}
+      labels={{
+        title: "Scout QA",
+        initial: "Hi there 👋, let's test your webapp today!",
+      }}
+      suggestions={suggestions}
+    />
+  );
+};
+
+export const Chat = () => {
   return (
     <CopilotKit
       agent={AGENT_ID_TEST_LEAD_AGENT}
       runtimeUrl="/api/copilotkit"
       showDevConsole={false}
     >
-      <CopilotChat
-        className="h-full"
-        RenderMessage={RenderMessage}
-        labels={{
-          title: "Scout QA",
-          initial: "Hi there 👋, let's test your webapp today!",
-        }}
-        suggestions={[
-          {
-            title: "AirBnb",
-            message: "Verify booking flow on airbnb.com",
-          },
-          {
-            title: "Google",
-            message: "Make sure user can see latest weather on google.com",
-          },
-          {
-            title: "Medium",
-            message:
-              "Make sure user can follow links on https://medium.com/blog/32-of-our-favorite-medium-stories-of-2023-1fb10ca34cd8",
-          },
-          {
-            title: "Temu",
-            message: "Testing buying a hoodie on temu.com",
-          },
-        ]}
-      />
+      <ChatInner />
     </CopilotKit>
   );
-}
+};
